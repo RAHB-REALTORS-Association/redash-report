5fd1f936e0920461f6f020380e897d04297d5f16
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2023-09-29 21:12:45 -0400

    Support multiple attachments

--- send_email.py
+++ send_email.py
@@ -1,31 +1,39 @@
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
+from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition, Personalization
 import base64
+import mimetypes
 
-def send_email(sendgrid_api_key, from_email, to_emails, subject, content, xlsx_file):
+
+def send_email(sendgrid_api_key, from_email, to_emails, subject, content, files):
     mail = Mail(
         from_email=from_email,
-        to_emails=to_emails,
         subject=subject,
         plain_text_content=content
     )
     
-    # Read the XLSX file in binary mode and encode it as Base64
-    with open(xlsx_file, "rb") as f:
-        encoded_file = base64.b64encode(f.read()).decode()
+    # Create a Personalization object and add all the recipients to it
+    personalization = Personalization()
+    for to_email in to_emails:
+        personalization.add_to(to_email)
+    mail.add_personalization(personalization)
     
-    # Create an Attachment object for the XLSX file
-    attachment = Attachment(
-        file_content=FileContent(encoded_file),
-        file_type=FileType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
-        file_name=FileName(xlsx_file),
-        disposition=Disposition("attachment")
-    )
+    # Handle multiple attachments
+    for file in files:
+        with open(file, "rb") as f:
+            encoded_file = base64.b64encode(f.read()).decode()
+
+        # Dynamically determine the file_type based on the file extension
+        file_type = mimetypes.guess_type(file)[0] or 'application/octet-stream'
+
+        attachment = Attachment(
+            file_content=FileContent(encoded_file),
+            file_type=FileType(file_type),  # Set the file_type dynamically
+            file_name=FileName(file),
+            disposition=Disposition("attachment")
+        )
+
+        mail.add_attachment(attachment)
     
-    # Add the attachment to the mail
-    mail.attachment = attachment
-    
-    # Create a SendGridAPIClient object and send the email
     client = SendGridAPIClient(sendgrid_api_key)
     response = client.send(mail)
     return response
